6e55f0f982c0eab9be8fd3338f3c53ff97b5032b
Rodrigo Barros Bernardino <rbbernardino@gmail.com>
2019-04-16 00:01:20 -0300

    split iterative calculations from matrix ones
    - for testing and checking if the matrix versions are correct

--- Aprendizagem de máquina/mvfuzzy.py
+++ Aprendizagem de máquina/mvfuzzy.py
@@ -39,30 +39,6 @@ def mvfuzzy3(D: np.array, K, m, T, err):
     return (G_medoids, U_membDegree)
 
 
-def calc_membership_degree_loop(D, G_medoids, W_weights, K, m):
-    n_elems = D.shape[0]
-    p_views = D.shape[2]
-
-    # initialize membership degree vector
-    U_membDegree = np.zeros(shape=[n_elems, K], dtype=float)
-    for i in range(0, n_elems):
-        for k in range(0, K):
-            u_ik = 0
-            # calc A_ik
-            A_ik = 0
-            for j in range(0, p_views):
-                A_ik += W_weights[k, j] * D[i, G_medoids[k, j], j]
-            # calc B_ik
-            for h in range(0, K):
-                B_ih = 0
-                for j in range(0, p_views):
-                    B_ih += W_weights[h, j] * D[i, G_medoids[h, j], j]
-                u_ik += (A_ik/B_ih)**(1/(m-1))
-            # calc final U_ik
-            U_membDegree[i, k] = 1/u_ik
-    return U_membDegree
-
-
 def calc_membership_degree(D, G_medoids, W_weights, K, m):
     n_elems = D.shape[0]
     p_views = D.shape[2]
